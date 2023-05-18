7d20515ff444c6c25393ca207bc9d274fc5dd69c
taofeek olamide <taofeekolamide55@gmail.com>
2023-05-18 12:44:55 +0100

    ola

--- script/index.js
+++ script/index.js
@@ -12,5 +12,27 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "images/o1.jpg");
     }
 }
+let myButton=document.querySelector("button");
+let myHeader=document.querySelector("h1");
+
+function setName(){
+    const myName = prompt("What is your name?");
+    localStorage.setItem("name", myName);
+    myHeader.textContent = `Welcome to Front-End Development, ${myName}`;
+    let myButton2=document.querySelector("button");
+    myButton2.textContent=`Thank you, ${myName}`;
+}
+
+if(!localStorage.getItem("name")){
+    setName();
+} 
+else{
+    const storedName = "";
+    myHeader.textContent = `Welcome to Front-End Development, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setName();
+  };
 
 
